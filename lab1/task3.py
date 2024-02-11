0b16fc17aea39fa287e27decd3bf4fed5aafe66a
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-11 14:32:06 +0300

    Update task3.py
    прописал условие и добавил функцию для вычисления количества слов

--- lab1/task3.py
+++ lab1/task3.py
@@ -1,1 +1,9 @@
+#8 Дана строка в которой все слова записаны через пробел. Необходимо посчитать количество слов с четным количеством знаков.
 
+def count_words(text):
+    words = text.split()
+    count = 0
+    for word in words:
+        if len(word) % 2 == 0:
+            count += 1
+    return count
