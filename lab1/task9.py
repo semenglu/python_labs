27192e32945a70a46b092f51954c5c0df940568a
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-17 23:35:58 +0300

    Update task9.py
    добавил решение к 9 задаче

--- lab1/task9.py
+++ lab1/task9.py
@@ -1,3 +1,17 @@
 #Задание 9 Прочитать список строк с клавиатуры. Упорядочить по длине строки.
 
+n = int(input("Введите количество строк: "))  
+strings = []
 
+# заполняем список строками, введенными пользователем
+for i in range(n):
+    string = input("Введите строку: ")
+    strings.append(string)
+
+# сортируем список строк по их длине
+sorted_strings = sorted(strings, key=len)
+
+# выводим отсортированные строки
+print("Строки, упорядоченные по длине:")
+for string in sorted_strings:
+    print(string)
