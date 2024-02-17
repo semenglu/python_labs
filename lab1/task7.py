4da60c30b6c5b15b1c4817e1b57a7e52b89d0325
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-17 23:02:09 +0300

    Update task7.py
    добавил решение к 7 задаче

--- lab1/task7.py
+++ lab1/task7.py
@@ -1,1 +1,12 @@
+#8 Дана строка. Необходимо найти все используемые в ней строчные символы латиницы.
 
+s = input("Введите строку: ")
+
+latin_lower_chars = set()
+for char in s:
+    if 'a' <= char <= 'z':
+        latin_lower_chars.add(char)
+
+print("Используемые строчные символы латиницы в строке:")
+for char in sorted(latin_lower_chars):
+    print(char, end=' ')
