23896a24fec469e0b685cae67f130b4ae03254fb
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-18 00:14:00 +0300

    Update task11.py
    добавил решение к задаче 11

--- lab1/task11.py
+++ lab1/task11.py
@@ -1,1 +1,25 @@
 #3 В порядке увеличения разницы между частотой наиболее часто встречаемого символа в строке и частотой его появления в алфавите.
+
+import string
+def calculate_frequency_difference(s):
+    s = s.lower()
+    char_count = [s.count(char) for char in string.ascii_lowercase if char in s]
+    if not char_count:
+        return 0
+    most_common_freq = max(char_count)
+    most_common_char = string.ascii_lowercase[char_count.index(most_common_freq)]
+
+    if most_common_char in string.ascii_lowercase:
+        alphabet_position = string.ascii_lowercase.index(most_common_char)
+        return most_common_freq - alphabet_position
+    else:
+        return 0
+
+
+n = int(input("Введите количество строк: "))
+strings = [input("Введите строку: ") for i in range(n)]
+sorted_strings = sorted(strings, key=calculate_frequency_difference)
+
+print("Строки, упорядоченные по разнице частоты символов:")
+for string in sorted_strings:
+    print(string)
