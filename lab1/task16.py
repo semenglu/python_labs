ce8dd7248e44d9519647324849c7827c980c682a
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-18 23:22:17 +0300

    Update task16.py
    решение к задаче 16

--- lab1/task16.py
+++ lab1/task16.py
@@ -1,1 +1,22 @@
 #15 Дан целочисленный массив и натуральный индекс (число, меньшее размера массива). Необходимо определить является ли элемент по указанному индексу локальным минимумом.
+def is_global_maximum(arr, index):
+    if index < len(arr):
+        return arr[index] == min(arr)
+    else:
+        return False
+
+size = int(input("Введите размер массива: "))
+arr = []
+
+print("Введите элементы массива:")
+
+for i in range(size):
+    element = int(input(f"Элемент {i}: "))
+    arr.append(element)
+
+index = int(input("Введите индекс: "))
+
+if is_global_maximum(arr, index):
+    print(f"Элемент в индексе {index} является глобальным минимумом.")
+else:
+    print(f"Элемент в индексе {index} не является глобальным минимумом.")
