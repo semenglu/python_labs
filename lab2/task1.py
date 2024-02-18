045f02c9f8212d97a133981597a99c482ba0a821
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-19 00:26:52 +0300

    Update task1.py
    решение к задаче 1

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,1 +1,7 @@
 #Даны два списка чисел. Найдите все числа, которые входят как в первый, так и во второй список и выведите их в порядке возрастания.
+
+lst1 = list(map(int, input().split()))
+lst2 = list(map(int, input().split()))
+
+result = sorted(list(set(lst1) & set(lst2)))
+print(" ".join(map(str, result)))
