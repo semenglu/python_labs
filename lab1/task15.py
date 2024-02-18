0537f47194dccc97c83c37e6b4c6d663cbd21317
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-18 23:08:05 +0300

    Update task15.py
    добавил решение к задаче 15

--- lab1/task15.py
+++ lab1/task15.py
@@ -1,1 +1,23 @@
 #3 Дан целочисленный массив и натуральный индекс (число, меньшее размера массива). Необходимо определить является ли элемент по указанном индексу глобальным максимумом.
+
+def is_global_maximum(arr, index):
+    if index < len(arr):
+        return arr[index] == max(arr)
+    else:
+        return False
+
+size = int(input("Введите размер массива: "))
+arr = []
+
+print("Введите элементы массива:")
+
+for i in range(size):
+    element = int(input(f"Элемент {i}: "))
+    arr.append(element)
+
+index = int(input("Введите индекс: "))
+
+if is_global_maximum(arr, index):
+    print(f"Элемент в индексе {index} является глобальным максимумом.")
+else:
+    print(f"Элемент в индексе {index} не является глобальным максимумом.")
