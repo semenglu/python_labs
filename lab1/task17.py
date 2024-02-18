9c673cb1643a90fb2b9f038762cee62b64058300
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-18 23:36:23 +0300

    Update task17.py
    решение к задаче 17

--- lab1/task17.py
+++ lab1/task17.py
@@ -1,1 +1,30 @@
 #27 Дан целочисленный массив. Необходимо осуществить циклический сдвиг элементов массива влево на одну позицию.
+
+def rotate_left(arr):
+    if len(arr) <= 1:
+        return arr
+
+    first_element = arr[0]
+
+    for i in range(len(arr) - 1):
+        arr[i] = arr[i + 1]
+
+    arr[-1] = first_element
+
+    return arr
+
+
+size = int(input("Введите размер массива: "))
+arr = []
+
+print("Введите элементы массива:")
+
+for i in range(size):
+    element = int(input(f"Элемент {i}: "))
+    arr.append(element)
+
+print("Исходный массив:", arr)
+
+# Вызов функции для циклического сдвига элементов влево
+rotated_array = rotate_left(arr)
+print("Массив после циклического сдвига влево:", rotated_array)
