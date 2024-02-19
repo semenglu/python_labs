0e837ce07173bf13b464f4c2794a093a369eeac1
Semen <112971765+semenglu@users.noreply.github.com>
2024-02-19 14:52:10 +0300

    Update task2.py
    решение к задаче 2

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,1 +1,16 @@
 #Выборы в США
+
+results = {}
+n = int(input())
+
+for i in range(n):
+    record = input().split()
+    for i in range(1, len(record)):
+        candidate, votes = record[i-1], int(record[i])
+        if candidate in results:
+            results[candidate] += votes
+        else:
+            results[candidate] = votes
+
+for candidate in sorted(results.keys()):
+    print(candidate, results[candidate])
